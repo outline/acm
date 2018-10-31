04e238d410fbc980f06f9e9e72473c9d1e54e7a6
Tom Moor <tom.moor@gmail.com>
2018-10-30 21:15:29 -0700

    Remove CLI install in favor of buildpack

--- cert.sh
+++ cert.sh
@@ -9,18 +9,16 @@ set -e
 # HEROKU_APP
 
 # Download dependencies
-curl https://get.acme.sh | sh
-curl https://cli-assets.heroku.com/install.sh | sh
-
-# Reload the environment
-source /etc/profile
+git clone https://github.com/Neilpang/acme.sh.git
+cd ./acme.sh
+./acme.sh --install --force
 
 # Map to environment variables that the ACME script requires
 export CF_Email=$CLOUDFLARE_EMAIL
 export CF_Key=$CLOUDFLARE_API_KEY
 
 # Generate wildcard certificate (this will take approx 130s)
-acme.sh  --issue -d $DOMAIN  -d "*.$DOMAIN"  --dns dns_cf
+~/.acme.sh/acme.sh  --issue -d $DOMAIN  -d "*.$DOMAIN"  --dns dns_cf
 
 # Update the certificate in the live app
 heroku certs:add "~/.acme.sh/$DOMAIN/$DOMAIN.cer" "~/.acme.sh/$DOMAIN/$DOMAIN.key" --app $HEROKU_APP
